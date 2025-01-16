29760d85121d4b4ae523760b671df40af5998dff
Thullyoo <thullyofilho@gmail.com>
2025-01-16 12:37:01 -0300

    feat: add IPersonRepository

---
+++ Data/Repository/IPersonRepository.cs
@@ -0,0 +1,16 @@
+﻿using crud_mysql.Request;
+
+namespace crud_mysql.Data.Repository;
+
+public interface IPersonRepository
+{
+    Task<Person> AddPerson(PersonRequest personRequest);
+    
+    Task<Person> UpdatePerson(Guid id, PersonRequest personRequest);
+    
+    void DeletePerson(Guid id);
+    
+    Task<Person> GetPersonById(Guid id);
+    
+    Task<List<Person>> GetAllPersons();
+}
