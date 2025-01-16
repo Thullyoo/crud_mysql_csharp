b7762e35ac7cccdb1161de560c9442f79fbd2c50
Thullyoo <thullyofilho@gmail.com>
2025-01-16 12:40:44 -0300

    feat: add PersonMapper

---
+++ Mapper/PersonMapper.cs
@@ -0,0 +1,11 @@
+﻿using crud_mysql.Request;
+
+namespace crud_mysql.Mapper;
+
+public static class PersonMapper
+{
+    public static Person MapRequestToPerson(PersonRequest personRequest)
+    {
+        return new Person(personRequest.email, personRequest.password);
+    }
+}
