c6b75a17623651eb2d664ed8297eb2c73352b90d
Thullyoo <thullyofilho@gmail.com>
2025-01-16 11:22:54 -0300

    feat: add Person Model

---
+++ Models/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace crud_mysql;
+
+public class Person
+{
+    public Guid Id { get; init; }
+    public string Email { get; private set; }
+    public string Password { get; private set; }
+
+    public Person(string email, string password)
+    {
+        Id = Guid.NewGuid();
+        Email = email;
+        Password = password;
+    }
+
+    public void setEmail(string email)
+    {
+        Email = email;
+    }
+
+    public void setPassword(string password)
+    {
+        Password = password;
+    }
+}
