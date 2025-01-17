97e5ed9e645a042e47665797db43aa47dd42dba9
Thullyoo <thullyofilho@gmail.com>
2025-01-17 11:13:40 -0300

    feat: add PersonService

---
+++ Service/IPersonService.cs
@@ -0,0 +1,12 @@
+﻿using crud_mysql.Request;
+
+namespace crud_mysql.Service;
+
+public interface IPersonService
+{
+    Task<List<Person>> GetPersons();
+    Task<Person> GetPersonById(Guid id);
+    Task<Person> AddPerson(PersonRequest person);
+    Task<Person> UpdatePerson(Guid id, PersonRequest person);
+    void DeletePerson(Guid id);
+}
--- obj/Debug/net9.0/crud_mysql.AssemblyInfo.cs
+++ obj/Debug/net9.0/crud_mysql.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("crud_mysql")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9feafb16753b04ccab41267d7829b886590fa30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a504bea532d196cfd9c0d1e3e46324cf5ba0a7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("crud_mysql")]
 [assembly: System.Reflection.AssemblyTitleAttribute("crud_mysql")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
