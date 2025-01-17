74d159253b24f5c5e1eae4aca6cd7cb59350f89a
Thullyoo <thullyofilho@gmail.com>
2025-01-17 10:29:12 -0300

    feat: add PersonControlle with end point to register Person

---
+++ Controller/PersonController.cs
@@ -0,0 +1,30 @@
+﻿using crud_mysql.Data.Repository;
+using crud_mysql.Request;
+using Microsoft.AspNetCore.Mvc;
+
+namespace crud_mysql.Controller;
+
+[ApiController]
+[Route("person")]
+public class PersonController : ControllerBase
+{
+    private readonly IPersonRepository _personRepository;
+
+    public PersonController(IPersonRepository personRepository)
+    {
+        _personRepository = personRepository;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(PersonRequest person)
+    {
+        var result = _personRepository.AddPerson(person);
+
+        if (result == null)
+        {
+            return BadRequest();
+        }
+
+        return Created("", person);
+    }
+}
