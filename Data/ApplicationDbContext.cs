a9b130a8a97471b3334bd0e6d906e0dccc2a27ad
Thullyoo <thullyofilho@gmail.com>
2025-01-17 10:27:36 -0300

    feat: add ApplicationDbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace crud_mysql.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+     public DbSet<Person> Persons { get; set; }
+}
